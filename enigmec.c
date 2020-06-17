c8fcab0381440222d1bcb147d9ceee098d7ded60
BeaNecib <61478352+BeaNecib@users.noreply.github.com>
2020-06-17 10:34:58 +0100

    Update enigmec.c

--- enigmec.c
+++ enigmec.c
@@ -4,23 +4,21 @@
 #include <SDL/SDL.h>
 #include <SDL/SDL_mixer.h>
 #include <SDL/SDL_image.h>
-#include "enigmeh.h"
+#include "enigme.h"
 
 void init_enigme(enigme * e)
 {
 	e->p.x=0;
-	e->p.y=0;
+	e->p.y=0;	
 	e->img=NULL;
 }
 
  void generate_afficher (SDL_Surface * screen  , char image [],enigme *e,int *alea)
 { 
 	int test=*alea ;
-do
-{
- *alea = 1+ rand()%6;
-}
-while(*alea==test) ;
+do{
+ *alea = 1+ rand()%3;
+}while(*alea==test) ;
  sprintf(image ,"%d.png",*alea);
 e->img = IMG_Load(image);
  SDL_BlitSurface(e->img,NULL,screen,&(e->p)) ;
@@ -34,8 +32,7 @@ e->img = IMG_Load(image);
  	if(strcmp(image,"1.png")==0)
  	{
      solution =3 ;
- 	(*ques)=1;
-}
+ 	(*ques)=1;}
  	else  	if(strcmp(image,"2.png")==0)
  	{
  		solution =2;
@@ -45,20 +42,17 @@ e->img = IMG_Load(image);
  	else 	if(strcmp(image,"3.png")==0)
  	{
  		solution =1;
-(*ques)=3;	
- 	}
- 	else 	if(strcmp(image,"4.png")==0)
- 	{
- 		solution =2;
-(*ques)=4;	
- 	}
- 	return solution;
+(*ques)=3;	 	}
+ 	
  }
-
+return solution;
+}
 int resolution (int * running,int *run )
 {
 	SDL_Event event ;
   int r=0 ;
+int doing=1;
+while(doing){
 	SDL_PollEvent(&event);
 	switch(event.type)
 	{
@@ -72,54 +66,56 @@ int resolution (int * running,int *run )
                 {
 			  case  SDLK_a: 
 			  r= 1;
+doing=0;
 			   break ;
-			   case  SDLK_z:
+			   case  SDLK_z :
 			   r= 2;
+doing=0;
 			   break;
 			   case SDLK_e: 
 			   r=3 ;
+doing=0;
 			   break;
 			    }
        break ;
 
                  
-	}
+	}}
   return r ;
 }
-
-void afficher_resultat (SDL_Surface * screen,int s,int r,enigme *en,int ques)
- {
-SDL_Surface * image=NULL;
- switch(ques)
-{
+ 
+ int afficher_resultat (SDL_Surface * screen,int s,int r,enigme *en,int ques)
+ {SDL_Surface * image=NULL;
+ switch(ques){
 case 1:
+
 image=IMG_Load("1e.png");
 break;
-
 case 2:
+
 image=IMG_Load("2e.png");
 break;
-
 case 3:
+
 image=IMG_Load("3e.png");
 break;
-
-case 4:
-image=IMG_Load("4e.png");
-break;
-
-	SDL_BlitSurface(image, NULL, screen, &(en=p)) ;
+}
+	SDL_BlitSurface(image, NULL, screen, &(en->p)) ;
         SDL_Flip(screen);
  	if (r==s)
  	{
-	en->img=IMG_Load("00.jpg"); // image de : vous avez repondu correctement
- 		SDL_BlitSurface(en=img, NULL, screen, &(en_p)) ;
+
+	en->img=IMG_Load("CORRECT.jpg");
+ 		SDL_BlitSurface(en->img, NULL, screen, &(en->p)) ;
         SDL_Flip(screen);
+return 1;
  		} 	
  	else
  	{
- 		en->img=IMG_Load("11.jpg"); // image de : vous avez répondu faux
+ 		en->img=IMG_Load("WRONG.jpg");
  		SDL_BlitSurface(en->img, NULL, screen, &(en->p)) ;
         SDL_Flip(screen);
+return 0;
  	}
  }
+ 
