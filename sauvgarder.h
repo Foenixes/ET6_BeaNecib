e92bdc72ffb2a5e37e1ef02aca405c64b6f76909
BeaNecib <61478352+BeaNecib@users.noreply.github.com>
2020-06-17 12:07:42 +0100

    Create sauvgarder.h

---
+++ sauvgarder.h
@@ -0,0 +1,24 @@
+/**
+* @file sauvegarde.h
+* @brief Testing Program.
+* @author C Team
+* @version 0.1
+* @date Apr 01, 2015
+*
+* structure perso
+*
+*/
+#ifndef SAUVEGARDE_H_INCLUDED
+#define SAUVEGARDE_H_INCLUDED
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL/SDL.h>
+#include <stdbool.h>
+#include <math.h>
+#include <enigme.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
+#include <SDL/SDL_mixer.h>
+
